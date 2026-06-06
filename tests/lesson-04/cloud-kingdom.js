59d2052ae6a9dc9f101a8083ee9c47af5553f3bf
test01 - HueLuu <luuhue998vp@gmail.com>
2026-06-06 16:06:32 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,10 @@
+let playerName = 'Mario';
+let currentLives = 3;
+const Level1 = 25;
+const Level2 = 30;
+const Level3 = 45;
+
+let totalCoins = Level1 + Level2 + Level3
+let averageCoin = totalCoins/3
+let soDu = totalCoins %3;
+console.log (totalCoins, averageCoin, soDu);
