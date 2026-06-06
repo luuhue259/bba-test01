ba57b10161ee1f86d841660a6f37448a5b5a6260
test01 - HueLuu <luuhue998vp@gmail.com>
2026-06-06 16:15:27 +0700

    feat: final stage

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,17 @@
+
+function findPairsDivisibleBy17() {
+    let count = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        for (let j = i + 1; j <= 100; j++) {
+            if ((i + j) % 17 === 0) {
+                console.log(`(${i}, ${j}) = ${i + j}`);
+                count++;
+            }
+        }
+    }
+
+    console.log(`\nTổng cộng: ${count} cặp`);
+}
+
+findPairsDivisibleBy17();
